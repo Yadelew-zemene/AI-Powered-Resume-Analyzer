ec6b12d967bc5a155e4c3405bcfc94124849d5c9
Yadelew-zemene <yadelewzemene@gmail.com>
2025-11-24 11:22:16 +0300

    first Commit

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,36 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/component/Navbar";
+import {resumes} from "../../constants";
+import resumeCard from "~/component/ResumeCard";
+import ResumeCard from "~/component/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "CVGenius" },
+    { name: "description", content: "Smart feedback for your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (<main className="bg-[url('./images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className='main-section'>
+          <div className ="Page-hadding">
+              <h1 >Track Your Application &  Resume Rating</h1>
+              <h2>Review your submissions and check AI-powered feedback.</h2>
+          </div>
+
+      </section>
+      {resumes.length > 0 && (
+          <div className="resume-section">
+              {resumes.map(resume => (
+                      <ResumeCard key={resume.id} resume={resume} />
+                  )
+
+
+              )}
+              </div>
+      )}
+
+      </main>)
 }
